2d2f4e01a7d03403fed01614aba4faeb13761a87
utsav7011 <72941769+utsav7011@users.noreply.github.com>
2020-10-19 08:27:50 +0530

    Update supermarkert bill.py

--- supermarkert bill.py
+++ supermarkert bill.py
@@ -2,7 +2,7 @@ class store:
     __itemcode=[]
     __price=[]
     def getd(self):
-        for i in range (5):
+        for i in range (10):
             self.__itemcode.append(int(input("\n Enter the item code : ")))
             self.__price.append(float(input("\n Enter price : ")))
 
@@ -15,7 +15,7 @@ class store:
          t=0
         
          print("\n ----------------------------------------------Bill---------------------------------------------------------")
-         print("\n********************************************************************")   
+       
          print("Itemcode\t\t Quantity \t\t price\item \t sub total " )    
          for i in range (5):
              print(self.__itemcode[i],"\t\t",quantity[i],"\t\t",self.__price[i],"\t\t",self.__price[i]*quantity[i])
